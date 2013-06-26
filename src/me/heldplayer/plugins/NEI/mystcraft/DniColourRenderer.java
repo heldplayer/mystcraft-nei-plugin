f7f96417e9df3d7673a60df09c1da93b4dce7fa2
Heldplayer <mitchel.pyl@gmail.com>
2013-06-26 13:08:45 +0200

    Change rendering of the D'ni eye a bit

--- src/me/heldplayer/plugins/NEI/mystcraft/DniColourRenderer.java
+++ src/me/heldplayer/plugins/NEI/mystcraft/DniColourRenderer.java
@@ -23,42 +23,42 @@ public class DniColourRenderer {
         int H = (int) (HSB[0] * 360.0F);
 
         double eyelidRadius = radius;
-        float eyeRadius = 1.0F + (1.0F - HSB[1]) * 3.0F;
+        float eyeRadius = (1.0F + (1.0F - HSB[1]) * 3.0F) * (float) radius / 20.0F;
 
         boolean outerEyelid = false;
         boolean innerEyelid = false;
         boolean eye = false;
-        if (HSB[1] > 0.3F && HSB[2] > 0.3F) {
+        if (HSB[1] > 0.3F && HSB[2] > 0.5F) {
             if (H <= 28 || H > 274) {
                 int temp = (H + 85) % 360;
-                eyelidRadius = MathHelper.abs(MathHelper.cos(((float) temp / 114.0F)) * radius * 4.0D / 3.0D);
+                eyelidRadius = MathHelper.abs(MathHelper.cos(((float) temp / 114.0F)) * radius);
                 outerEyelid = true;
                 innerEyelid = true;
             }
             else if (H <= 114) {
                 int temp = H - 30;
                 outerEyelid = true;
-                eyelidRadius = MathHelper.abs(MathHelper.sin(((float) temp / 86.0F)) * radius * 4.0D / 3.0D);
+                eyelidRadius = MathHelper.abs(MathHelper.sin(((float) temp / 86.0F)) * radius);
                 eye = true;
             }
             else if (H <= 274) {
                 int temp = H - 114;
                 innerEyelid = true;
-                eyelidRadius = MathHelper.abs(MathHelper.cos(((float) temp / 160.0F)) * radius * 4.0D / 3.0D);
+                eyelidRadius = MathHelper.abs(MathHelper.cos(((float) temp / 160.0F)) * radius);
                 eye = true;
             }
         }
         else {
-            float mod = MathHelper.min(HSB[1], HSB[2]) / 0.3F;
-            eyeRadius *= HSB[2] / 0.3F;
+            float mod = MathHelper.min(HSB[1] / 0.3F, HSB[2] / 0.5F);
+            eyeRadius *= HSB[2] / 0.5F;
             if (H <= 28 || H > 274) {
                 int temp = (H + 85) % 360;
                 if (mod < 0.3F) {
-                    eyelidRadius = MathHelper.abs(MathHelper.max(MathHelper.cos(((float) temp / 114.0F)), 1.0F - mod / 0.3F) * radius * 4.0D / 3.0D);
+                    eyelidRadius = MathHelper.abs(MathHelper.max(MathHelper.cos(((float) temp / 114.0F)), 1.0F - mod / 0.3F) * radius);
                     eye = true;
                 }
                 else {
-                    eyelidRadius = MathHelper.abs(MathHelper.min(MathHelper.cos(((float) temp / 114.0F)), (mod - 0.3F) / 0.7F) * radius * 4.0D / 3.0D);
+                    eyelidRadius = MathHelper.abs(MathHelper.min(MathHelper.cos(((float) temp / 114.0F)), (mod - 0.3F) / 0.7F) * radius);
                     innerEyelid = true;
                 }
                 outerEyelid = true;
@@ -66,18 +66,24 @@ public class DniColourRenderer {
             else if (H <= 114) {
                 int temp = H - 30;
                 outerEyelid = true;
-                eyelidRadius = MathHelper.abs(MathHelper.max(MathHelper.cos(((float) temp / 86.0F)), 1.0F - mod) * radius * 4.0D / 3.0D);
+                eyelidRadius = MathHelper.abs(MathHelper.max(MathHelper.cos(((float) temp / 86.0F)), 1.0F - mod) * radius);
                 eye = true;
             }
             else if (H <= 274) {
                 int temp = H - 114;
                 innerEyelid = true;
-                eyelidRadius = MathHelper.abs(MathHelper.max(MathHelper.cos(((float) temp / 160.0F)), 1.0F - mod) * radius * 4.0D / 3.0D);
+                eyelidRadius = MathHelper.abs(MathHelper.max(MathHelper.cos(((float) temp / 160.0F)), 1.0F - mod) * radius);
                 eye = true;
             }
         }
 
-        int max = 30;
+        if (eyeRadius > eyelidRadius) {
+            eyeRadius = (float) eyelidRadius;
+        }
+
+        eyelidRadius *= 4.0D / 3.0D;
+
+        int max = 20;
 
         Vector top = VectorPool.getFreeVector(center.posX, center.posY - radius, center.posZ);
         Vector bottom = VectorPool.getFreeVector(center.posX, center.posY + radius, center.posZ);
