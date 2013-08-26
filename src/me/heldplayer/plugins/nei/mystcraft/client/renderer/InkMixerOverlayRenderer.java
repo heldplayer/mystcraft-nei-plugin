43d91cb9dbbce36168c18e37d179f23d80f5c3ba
Heldplayer <mitchel.pyl@gmail.com>
2013-08-26 17:37:59 +0200

    Fix some rendering

--- src/me/heldplayer/plugins/nei/mystcraft/client/renderer/InkMixerOverlayRenderer.java
+++ src/me/heldplayer/plugins/nei/mystcraft/client/renderer/InkMixerOverlayRenderer.java
@@ -2,6 +2,7 @@
 package me.heldplayer.plugins.nei.mystcraft.client.renderer;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import net.minecraft.inventory.Slot;
 
@@ -25,7 +26,7 @@ public class InkMixerOverlayRenderer implements IRecipeOverlayRenderer {
     private ArrayList<PositionedStack> ingreds;
     private PositionedStack ingredient;
 
-    public InkMixerOverlayRenderer(ArrayList<PositionedStack> stacks, IStackPositioner positioner, PositionedStack ingredient) {
+    public InkMixerOverlayRenderer(List<PositionedStack> stacks, IStackPositioner positioner, PositionedStack ingredient) {
         this.positioner = positioner;
         this.ingreds = new ArrayList<PositionedStack>();
         for (PositionedStack stack : stacks) {
@@ -44,22 +45,24 @@ public class InkMixerOverlayRenderer implements IRecipeOverlayRenderer {
     public void renderOverlay(GuiContainerManager gui, Slot slot) {
         GL11.glEnable(GL11.GL_BLEND);
         GL11.glBlendFunc(GL11.GL_SRC_ALPHA, GL11.GL_ONE);
-        GL11.glColor4d(0.6, 0.6, 0.6, 0.7);
 
         GuiContainerManager.setColouredItemRender(true);
 
-        if (slot.slotNumber == 0) {
-            GuiContainerManager.drawItem(this.ingredient.relx, this.ingredient.rely, this.ingredient.item);
-        }
-
         for (PositionedStack stack : this.ingreds) {
             if (stack.relx == slot.xDisplayPosition && stack.rely == slot.yDisplayPosition) {
+                GL11.glColor4d(0.6F, 0.6F, 0.6F, 0.7F);
                 GuiContainerManager.drawItem(stack.relx, stack.rely, stack.item);
             }
         }
+
+        if (slot.slotNumber == 0) {
+            GL11.glColor4d(0.6F, 0.6F, 0.6F, 0.7F);
+            GuiContainerManager.drawItem(this.ingredient.relx, this.ingredient.rely, this.ingredient.item);
+        }
+
         GuiContainerManager.setColouredItemRender(false);
 
-        GL11.glColor4f(1, 1, 1, 1);
+        GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
         GL11.glDisable(GL11.GL_BLEND);
         GL11.glEnable(GL11.GL_LIGHTING);
     }
